ae89bf26cf1a71797ef9527c94326f0cfa8e7a2a
bsquiz <48450530+bsquiz@users.noreply.github.com>
2022-02-28 07:37:19 -0600

    Delete objects directory

--- objects/Coin.js
+++
@@ -1,9 +0,0 @@
-class Coin extends Pickup {
-	constructor() {
-		super(Utils.PickupType.COIN);
-		this.width = 16;
-		this.height = 16;
-		this.MAX_FRAMES = 3;
-		this.points = 100;
-	}
-}
--- objects/Triangle.js
+++
@@ -1,7 +0,0 @@
-class Triangle extends Enemy {
-	constructor(originalX, originalY, behavior) {
-		super(originalX, originalY, behavior);
-		this.type = Utils.EnemyType.TRIANGLE;
-		this.maxSpeed = 2;
-	}
-}
